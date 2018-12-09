4302a53456954d84152f4c70a379198f02c67dc8
vladislav <ve@incsub.com>
2018-12-09 09:10:47 +0100

    Initial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+	let dir = Path::new("/home/ve/Env/wpd/projects/plugins/shipper");
+	if let Ok(entries) = fs::read_dir(dir) {
+		for entry in entries {
+			if let Ok(entry) = entry {
+				let path = entry.path();
+				println!("{:?} {}", path, path.is_dir());
+			}
+		}
+	} else {
+		println!( "Unable to read dir {:?}", dir );
+	}
 }
