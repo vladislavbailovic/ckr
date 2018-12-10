39ffd1091aef24624c548843347fffd48e1d2c4b
vladislav <ve@incsub.com>
2018-12-10 16:46:29 +0100

    Recurse through directory, read stuff along the way

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
 use std::fs;
 use std::path::Path;
 
-fn main() {
-	let dir = Path::new("/home/ve/Env/wpd/projects/plugins/shipper");
-	if let Ok(entries) = fs::read_dir(dir) {
-		for entry in entries {
-			if let Ok(entry) = entry {
-				let path = entry.path();
-				println!("{:?} {}", path, path.is_dir());
+fn list_dir(path: &str) {
+	let forbidden = [ "node_modules", ".git" ];
+	let dir = Path::new(path);
+	let mut process_path = true;
+	for &check in &forbidden {
+		if dir.ends_with(check) {
+			process_path = false;
+		}
+	}
+
+	if !process_path {
+		return
+	}
+
+	println!("---------------- Directory: {} -----------------------", path);
+	let entries = fs::read_dir(dir)
+		.expect( "Unable to read dir" );
+
+	for entry in entries {
+		if let Ok(entry) = entry {
+			let path = entry.path();
+			if path.is_dir() {
+				list_dir(path.to_str().expect("Invalid entry"));
+			} else {
+				println!("{:?}", path);
 			}
 		}
-	} else {
-		println!( "Unable to read dir {:?}", dir );
 	}
+}
+
+fn main() {
+	list_dir("/home/ve/Env/wpd/projects/plugins/shipper");
 }
