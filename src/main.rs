e49a170e0b36938e044e5c0bb405c6a9de00d41d
vladislav <ve@incsub.com>
2018-12-14 05:42:00 +0100

    Refactor dir processing

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,35 @@ use std::path::Path;
 
 fn process_file(filepath: &str) {
 	let path = Path::new(filepath);
-	let file = path.file_name().expect("Wut");
-	println!("{:?}", file);
+	let filename = path.file_name().expect("Wut");
+	
+	let contents = fs::read_to_string(filepath).unwrap();
+	println!("{}: {} chars", filename.to_str().unwrap(), contents.len());
 }
 
-fn list_dir(path: &str) {
+fn is_skip_dir(dir: &Path) -> bool {
 	let forbidden = [ "node_modules", ".git" ];
-	let dir = Path::new(path);
-	let mut process_path = true;
 	for &check in &forbidden {
 		if dir.ends_with(check) {
-			process_path = false;
+			return true;
 		}
 	}
 
-	if !process_path {
-		return
-	}
+	return false;
+}
 
-	let entries = fs::read_dir(dir)
-		.expect( "Unable to read dir" );
-
-	entries
+fn process_dir(dir: &Path) {
+	fs::read_dir(dir)
+		.unwrap()
 		.filter_map(Result::ok)
 		.filter(|e| e.path().is_dir())
 		.for_each(|e| list_dir(e.path().to_str().expect("Invalid entry")))
 	;
+}
 
+fn process_files(dir: &Path) -> u32 {
 	let mut files = 0;
+
 	fs::read_dir(dir)
 		.unwrap()
 		.filter_map(Result::ok)
@@ -44,6 +45,18 @@ fn list_dir(path: &str) {
 			process_file(f.path().to_str().expect("Invalid path"));
 		})
 	;
+
+	return files;
+}
+
+fn list_dir(path: &str) {
+	let dir = Path::new(path);
+	if is_skip_dir(dir) {
+		return;
+	}
+
+	process_dir(dir);
+	let files = process_files(dir);
 
 	if files > 0 {
 		println!("- {} files found in directory: {}", files, path);
