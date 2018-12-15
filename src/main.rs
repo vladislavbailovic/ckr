ef884b8129fadb5775c0b1e7eed12d2f39fe0020
vladislav <ve@incsub.com>
2018-12-15 04:53:52 +0100

    Refactor filter matchers

--- src/main.rs
+++ src/main.rs
@@ -12,47 +12,14 @@ fn main() {
 }
 
 fn list_dir(path: &str) -> Vec<PathBuf> {
-	let skip_dirs = [ ".git", "node_modules", "build", "dist"];
-	let file_types = [ "php", "js", "scss", "css" ];
+	let skip_dirs = vec![ ".git", "node_modules", "build", "dist"];
+	let file_types = vec![ "php", "js", "scss", "css" ];
 	let mut files = Vec::new();
 	let entries = WalkDir::new(path).into_iter()
 		.filter_map(|e| e.ok())
-		.filter(|e| {
-			// Filter blacklisted directories - skip the excluded ones.
-			let path = e.path();
-			if path.is_dir() {
-				return false;
-			}
-			
-			let dirname = path.to_str().unwrap();
-			for dir in skip_dirs.iter() {
-				let skip_path = format!("/{}/", dir);
-				if dirname.contains(skip_path.as_str()) {
-					return false;
-				}
-			}
-			
-			return true;
-		})
-		.filter(|e| {
-			// Filter whitelisted file types - only the included file types.
-			let path = e.path();
-			if path.is_dir() {
-				return false;
-			}
-
-			let extension = match path.extension() {
-				None => "",
-				Some(a) => a.to_str().unwrap(),
-			};
-			for file_type in file_types.iter() {
-				if extension == *file_type {
-					return true;
-				}
-			}
-			
-			return false;
-		})
+		.filter(|e| !e.path().is_dir())
+		.filter(|e| !is_in_blacklisted_dir(e.path(), &skip_dirs))
+		.filter(|e| has_whitelisted_extension(e.path(), &file_types))
 	;
 	for entry in entries {
 		files.push(entry.path().to_path_buf());
@@ -60,6 +27,33 @@ fn list_dir(path: &str) -> Vec<PathBuf> {
 
 	return files;
 }
+
+fn is_in_blacklisted_dir(path: &Path, blacklist: &Vec<&str>) -> bool {
+	let dirname = path.to_str().unwrap();
+	for dir in blacklist.iter() {
+		let skip_path = format!("/{}/", dir);
+		if dirname.contains(skip_path.as_str()) {
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+fn has_whitelisted_extension(path: &Path, whitelist: &Vec<&str>) -> bool {
+	let extension = match path.extension() {
+		None => "",
+		Some(a) => a.to_str().unwrap(),
+	};
+	for file_type in whitelist.iter() {
+		if extension == *file_type {
+			return true;
+		}
+	}
+	
+	return false;
+}
+
 
 fn process_file(filepath: &str) {
     let path = Path::new(filepath);
