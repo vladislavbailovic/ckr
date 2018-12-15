801f08f35c77d97b947f54de85c9fbd23eadda10
vladislav <ve@incsub.com>
2018-12-15 03:39:08 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,79 @@
+extern crate glob;
+
 use std::fs;
 use std::path::Path;
+use walkdir::WalkDir;
 
 fn main() {
-	list_dir("/home/ve/Env/wpd/projects/plugins/shipper");
+    list_dir("/home/ve/Env/wpd/projects/plugins/shipper");
 }
 
 fn list_dir(path: &str) {
-	let dir = Path::new(path);
-	if is_skip_dir(dir) {
-		return;
-	}
-
-	process_dir(dir);
-	process_files(dir);
-}
-
-fn is_skip_dir(dir: &Path) -> bool {
-	let forbidden = [ "node_modules", ".git" ];
-	for &check in &forbidden {
-		if dir.ends_with(check) {
-			return true;
-		}
-	}
-
-	return false;
-}
-
-fn process_dir(dir: &Path) {
-	fs::read_dir(dir)
-		.unwrap()
-		.filter_map(Result::ok)
-		.filter(|e| e.path().is_dir())
-		.for_each(|e| list_dir(e.path().to_str().expect("Invalid entry")))
-	;
-}
-
-fn process_files(dir: &Path) -> u32 {
-	let mut files = 0;
-
-	fs::read_dir(dir)
-		.unwrap()
-		.filter_map(Result::ok)
+	let skip_dirs = [ ".git", "node_modules", "build", "dist"];
+	let files = WalkDir::new(path).into_iter()
+		.filter_map(|e| e.ok())
 		.filter_map(|e| {
-			e.path().to_str().and_then(|f| {
-				if f.ends_with("php") { Some(e) } else { None }
-			})
-		})
-		.for_each(|f| {
-			files += 1;
-			process_file(f.path().to_str().expect("Invalid path"));
-		})
-	;
+			let path = e.path();
+			if !path.is_dir() {
+				// Files filtered separately.
+				return None;
 
-	return files;
+			match 
+			return Some(e);
+		})
+		.collect::<Vec<_>>()
+	;
+	println!("{:#?}", files);
 }
 
 fn process_file(filepath: &str) {
-	let path = Path::new(filepath);
-	let filename = path.file_name().expect("Wut");
-	
-	let contents = fs::read_to_string(filepath).unwrap();
-	let todos = get_todos_in_file(path, contents);
+    let path = Path::new(filepath);
+    let filename = path.file_name().expect("Wut");
 
-	if todos.len() > 0 {
-		println!(
-			"--- {}: {} todos ---",
-			filename.to_str().unwrap(),
-			todos.len()
-		);
-		for todo in todos {
-			println!("{:?}", todo);
-		}
-	}
+    let contents = fs::read_to_string(filepath).unwrap();
+    let todos = get_todos_in_file(path, contents);
+
+    if todos.len() > 0 {
+        println!(
+            "--- {}: {} todos ---",
+            filename.to_str().unwrap(),
+            todos.len()
+        );
+        for todo in todos {
+            println!("{:?}", todo);
+        }
+    }
 }
 
 fn get_todos_in_file(path: &Path, content: String) -> Vec<Todo> {
-	let mut todos = Vec::new();
-	let todo_str = "TODO";
-	content.lines()
-		.enumerate().for_each(|(idx, line)| {
-			if line.contains(todo_str) {
-				let char_pos = line.find(todo_str).unwrap();
-				let line = line
-					.replace("/*", "")
-					.replace("*", "")
-					.replace("*/", "")
-					.replace("//", "")
-					.replace("@", "")
-				;
-				todos.push(Todo {
-					path: path.to_str().unwrap().to_string(),
-					line: idx,
-					char: char_pos,
-					todo: line.trim().to_string(),
-					context: String::new(),
-				});
-			}
-		})
-	;
-	return todos;
+    let mut todos = Vec::new();
+    let todo_str = "TODO";
+    content.lines().enumerate().for_each(|(idx, line)| {
+        if line.contains(todo_str) {
+            let char_pos = line.find(todo_str).unwrap();
+            let line = line
+                .replace("/*", "")
+                .replace("*", "")
+                .replace("*/", "")
+                .replace("//", "")
+                .replace("@", "");
+            todos.push(Todo {
+                path: path.to_str().unwrap().to_string(),
+                line: idx,
+                char: char_pos,
+                todo: line.trim().to_string(),
+                context: String::new(),
+            });
+        }
+    });
+    return todos;
 }
 
 #[derive(Debug)]
 struct Todo {
-	path: String,
-	line: usize,
-	char: usize,
-	todo: String,
-	context: String,
+    path: String,
+    line: usize,
+    char: usize,
+    todo: String,
+    context: String,
 }
